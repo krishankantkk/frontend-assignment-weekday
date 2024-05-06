762fd3e777bddff1c90f52c0af8b5a9c53766f61
krishan kant <kantk1508@gmail.com>
2024-05-06 13:04:22 +0530

    filters

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
-import "./App.css";
+import React, { useState } from "react";
+import { Container } from "@material-ui/core";
+import Filters from "./components/Filters";
+import JobList from "./components/JobList";
+import "./index.css";
+const App = () => {
+  const [filters, setFilters] = useState({
+    minExperience: "",
+    companyName: "",
+    location: "",
+    techStack: "",
+    role: "",
+    minBasePay: "",
+    remoteOnSite: "",
+  });
 
-function App() {
-  return <div></div>;
-}
+  const handleApplyFilters = (newFilters) => {
+    setFilters(newFilters);
+  };
+
+  return (
+    <Container>
+      <Filters onApplyFilters={handleApplyFilters} />
+      <JobList filters={filters} />
+    </Container>
+  );
+};
 
 export default App;
